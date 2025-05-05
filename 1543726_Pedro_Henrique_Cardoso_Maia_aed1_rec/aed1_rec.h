104a472ac680637f8676d7782504e79a29da2a8b
Pedro Maia <150867861+pedro-maia1337@users.noreply.github.com>
2025-05-05 15:20:35 -0300

    Update aed1_rec.h

--- 1543726_Pedro_Henrique_Cardoso_Maia_aed1_rec/aed1_rec.h
+++ 1543726_Pedro_Henrique_Cardoso_Maia_aed1_rec/aed1_rec.h
@@ -13,5 +13,18 @@ int testar_simbolo_aritmetico  (  char str );
 int testar_simbolo_relacional  (  char str );
 int testar_simbolo_separador   (  char str );
 
+float maior_entre_tres(float a, float b, float c);
+
+int ordem_crescente_num(float x, float y, float z);
+
+// Função para verificar se está em ordem decrescente
+int ordem_decrescente_num(float x, float y, float z) (x > y) && (y > z);
+}
+
+int ordem_crescente(char x[], char y[], char z[]);
+
+// Função para verificar ordem alfabética decrescente
+int ordem_decrescente(char x[], char y[], char z[]); 
+
 
 #endif
