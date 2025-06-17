ffed82f18730b4b3875735ff6ddd146fcaebf175
Pedro Henrique Cardoso Maia <1543726@sga.pucminas.br>
2025-06-17 10:36:49 -0300

    add ed_15

---
+++ 1543726_Pedro_Henrique_Cardoso_Maia_aed1_ed15/mycat.hpp
@@ -0,0 +1,32 @@
+#ifndef _MYCAT_HPP_
+#define _MYCAT_HPP_
+
+#include <iostream>
+
+using std::cin ; // para entrada
+using std::cout; // para saida
+using std::endl; // para mudar de linha
+#include <iomanip>
+using std::setw; // para definir espacamento
+#include <string>
+using std::string; // para cadeia de caracteres
+#include <fstream>
+using std::ofstream; // para gravar arquivo
+using std::ifstream ; // para ler arquivo
+#include <ctime>
+#include <random>
+
+template < typename T >
+
+
+class Cat {
+    public: 
+        char* str_push_back ( char *s, char c ) {
+            //@return apontador para a cadeia atualizada
+            //@param s - apontador para cadeia de caracteres
+            //@param c - caractere a ser inserido
+        }
+
+};
+
+#endif
